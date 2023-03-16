8c180dd87b23bfe995c2dd9f1c6fd546865ffc6e
thosoo <mail@thaison-nguyen.de>
2023-03-16 10:18:25 +0100

    Update win32.bat

--- scrcpyPortable/App/win32.bat
+++ scrcpyPortable/App/win32.bat
@@ -1,4 +1,4 @@
 @echo off
-.\win32\scrcpy-win32-v1.25\scrcpy.exe %* && .\win32\scrcpy-win32-v1.25\adb kill-server
+.\win32\scrcpy-win32-v2.0\scrcpy.exe %* && .\win32\scrcpy-win32-v2.0\adb kill-server
 :: if the exit code is >= 1, then pause
 if errorlevel 1 pause
