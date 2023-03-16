f90c0d0e0e6b627ed880ec933d6a7043c1dcd6c9
thosoo <mail@thaison-nguyen.de>
2023-03-16 10:20:04 +0100

    Update win64.bat

--- scrcpyPortable/App/win64.bat
+++ scrcpyPortable/App/win64.bat
@@ -1,4 +1,4 @@
 @echo off
-.\win64\scrcpy-win64-v1.25\scrcpy.exe %* && .\win64\scrcpy-win64-v1.25\adb kill-server
+.\win64\scrcpy-win64-v2.0\scrcpy.exe %* && .\win64\scrcpy-win64-v2.0\adb kill-server
 :: if the exit code is >= 1, then pause
 if errorlevel 1 pause
