4452b6b68706cad086075e720b52a30e9cb3ac9c
thosoo <mail@thaison-nguyen.de>
2023-03-25 10:37:55 +0100

    Refactor

--- build.ps1
+++ build.ps1
@@ -1,7 +1,18 @@
-dir
-$name = Split-Path -Path (Get-Location) -Leaf
+Get-ChildItem
+
+$name = (Get-Location).Path.Split('\')[-1]
+$launcherGeneratorPath = Join-Path "D:\a" $name
+$launcherGeneratorPath = Join-Path $launcherGeneratorPath $name
+$launcherGeneratorPath = Join-Path $launcherGeneratorPath "PortableApps.comLauncher\PortableApps.comLauncherGenerator.exe"
+
+$installerGeneratorPath = Join-Path "D:\a" $name
+$installerGeneratorPath = Join-Path $installerGeneratorPath $name
+$installerGeneratorPath = Join-Path $installerGeneratorPath "PortableApps.comInstaller\PortableApps.comInstaller.exe"
+
 Write-Host "Starting Launcher Generator"
-Start-Process "D:\a\$name\$name\PortableApps.comLauncher\PortableApps.comLauncherGenerator.exe" -ArgumentList "D:\a\$name\$name\$name" -NoNewWindow -Wait
+Start-Process -FilePath $launcherGeneratorPath -ArgumentList "D:\a\$name\$name\$name" -NoNewWindow -Wait
+
 Write-Host "Starting Installer Generator"
-Start-Process "D:\a\$name\$name\PortableApps.comInstaller\PortableApps.comInstaller.exe" -ArgumentList "D:\a\$name\$name\$name" -NoNewWindow -Wait
-dir
+Start-Process -FilePath $installerGeneratorPath -ArgumentList "D:\a\$name\$name\$name" -NoNewWindow -Wait
+
+Get-ChildItem
