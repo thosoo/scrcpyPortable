4bd5b2083ec0eda9794c480ed51970a57d6b3446
thosoo <mail@thaison-nguyen.de>
2023-03-26 09:49:40 +0200

    Refactor

--- check-version.ps1
+++ check-version.ps1
@@ -1,32 +1,29 @@
+# Define repository name and API endpoint
 $repoName = "Genymobile/scrcpy"
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
-$tag = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).tag_name
-$tag2 = $tag
-Write-Host $tag2
-if ($tag2 -match "alpha")
-{
-  Write-Host "Found alpha."
-  echo "SHOULD_BUILD=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-}
-elseif ($tag2 -match "beta")
-{
-  Write-Host "Found beta."
-  echo "SHOULD_BUILD=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-}
-elseif ($tag2 -match "RC")
-{
-  Write-Host "Found Release Candidate."
-  echo "SHOULD_BUILD=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-}
-else{
-    echo "UPSTREAM_TAG=$tag2" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+
+# Retrieve latest tag from API endpoint
+$tag = (Invoke-RestMethod -Uri $releasesUri).tag_name
+
+# Print the tag to the console
+Write-Host $tag
+
+# Check if tag contains "alpha", "beta", or "RC"
+if ($tag -match "alpha|beta|RC") {
+    # If tag contains one of these strings, set SHOULD_BUILD to "no"
+    Write-Host "Found alpha, beta, or RC."
+    echo "SHOULD_BUILD=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+} else {
+    # If tag does not contain one of these strings, check if it differs from the local tag
+    echo "UPSTREAM_TAG=$tag" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
     $repoName2 = "thosoo/scrcpyPortable"
     $releasesUri2 = "https://api.github.com/repos/$repoName2/releases/latest"
-    $local_tag = (Invoke-WebRequest $releasesUri2 | ConvertFrom-Json).tag_name
-    if ($local_tag -ne $tag2){
+    $local_tag = (Invoke-RestMethod -Uri $releasesUri2).tag_name
+    if ($local_tag -ne $tag) {
+        # If local tag is different from upstream tag, set SHOULD_BUILD to "yes"
         echo "SHOULD_BUILD=yes" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-    }
-    else{
+    } else {
+        # If local tag is the same as upstream tag, set SHOULD_BUILD to "no"
         Write-Host "No changes."
         echo "SHOULD_BUILD=no" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
     }
