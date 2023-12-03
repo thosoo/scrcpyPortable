247ce85cd22d5cba2689fe6fa6a5c63d1ce67d3f
thosoo <thosoo@users.noreply.github.com>
2023-12-03 00:40:43 +0000

    2.3.1

--- scrcpyPortable/App/win32.bat
+++ scrcpyPortable/App/win32.bat
@@ -1,2 +1,2 @@
-@echo off`r`n.\win64\scrcpy-win64-v2.3\\scrcpy.exe %* && .\win64\scrcpy-win64-v2.3\\adb kill-server
+@echo off`r`n.\win64\scrcpy-win64-v2.3.1\\scrcpy.exe %* && .\win64\scrcpy-win64-v2.3.1\\adb kill-server
 :: if the exit code is >= 1, then pause`r`nif errorlevel 1 pause`r`n
